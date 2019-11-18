3c705a9d5ae3b4951abf10413e07315034492288
intfreedom <liuwenjunxjtu@163.com>
2019-11-18 16:42:14 +0800

    first commit

---
+++ GUI/Demo/myemit.h
@@ -0,0 +1,19 @@
+#ifndef MYEMIT_H
+#define MYEMIT_H
+
+#include<QObject>   //QObject是当前的基类
+extern double X, Y;
+
+class MyEmit:public QObject
+{
+    Q_OBJECT    //Q_OBJECT 提供Qt中的信号和槽；
+public:
+    explicit MyEmit(QObject *parent = nullptr);
+    void send_emit();
+
+signals:
+    void start_emit(double, int);
+public slots:
+};
+
+#endif // MYEMIT_H
